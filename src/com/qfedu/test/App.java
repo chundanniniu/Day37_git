2a413cc5fdf4ebc14854803b50da4d1a0c58f8b7
chundanniniu <51064555+chundanniniu@users.noreply.github.com>
2019-05-28 14:07:07 +0800

    Update App.java

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -3,7 +3,7 @@ package com.qfedu.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("first info");
-        //System.out.println("second");
+        System.out.println("second");
         System.out.println("three");
         System.out.println("four");
         
