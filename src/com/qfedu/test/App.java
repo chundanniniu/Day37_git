157dd831a30ceebd7266b51814f4055d0dc2aa36
chundanniniu <51064555+chundanniniu@users.noreply.github.com>
2019-05-28 11:42:26 +0800

    Update App.java

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("second");
+        System.out.println("three");
     }
 }
